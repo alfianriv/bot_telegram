a9c1726a901d270d086930677434efa3d0f3dc5e
Alfian <alfian.rivaldi8@gmail.com>
2019-04-22 15:54:30 +0700

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+const Telegram = require('node-telegram-bot-api');
+const dbot = require('dbot-js')
+
+const token = '839511924:AAH7t3zVeKKrv8RKdWtxkEs77fjpNRxUIME'
+const ksatria_bot = new TelegramBot(token, { polling: true });
+
+ksatria_bot.on('message', function(msg) {
+    var chatId = msg.chat.id;
+    var message = msg.text.toString()
+
+dbot.get_response(message, function(err, result){
+     if (!err) {
+      ksatria_bot.sendMessage(chatId, result)
+     }
+  })
+})
