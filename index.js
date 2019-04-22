e9a98fc3db3f1a312c80aca966f4cceb058fc332
Alfian <alfian.rivaldi8@gmail.com>
2019-04-22 14:57:30 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
-  res.end(`Hello from Node.js on Now 3.0!`);
-};
+const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
+
+const token = 'TELEGRAM_TOKEN';
+const simsimiKey = 'SIMSIMI_KEY';
+const url = `http://sandbox.api.simsimi.com/request.p?key=${simsimiKey}&lc=id&ft=1.0&text=`;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    request(url + msg.text.toString(), (err, response, body) => {
+        bot.sendMessage(chatId, JSON.parse(body).response);
+    });
+});
