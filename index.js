bf15bfef7613835c0676c083ceb2adb199b92ea7
Alfian <alfian.rivaldi8@gmail.com>
2019-04-23 10:34:49 +0700

    test

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const Telegram = require('node-telegram-bot-api');
-const dbot = require('dbot-js')
-
+const Bot = require('node-telegram-bot-api');
 const token = '839511924:AAH7t3zVeKKrv8RKdWtxkEs77fjpNRxUIME'
-const ksatria_bot = new Telegram(token, { polling: true });
+const bot = new Bot(token, {polling: true});
 
-ksatria_bot.on('message', function(msg) {
-  console.log(msg)
-    var chatId = msg.chat.id;
-    var message = msg.text.toString()
+bot.on('message', (msg) => {
+  console.log(msg);
 
-dbot.get_response(message, function(err, result){
-     if (!err) {
-      ksatria_bot.sendMessage(chatId, result)
-     }
-  })
-})
-
-require('http')
-  .createServer((req, res) => {
-    res.end('Hello from a vanilla server!')
-  })
-  .listen()
+  bot.sendMessage(msg.chat.id, msg.text.toString());
+});
